775acf5e741bc07cb1fbaa6adda2b13b1035b710
todydanielm3 <todydanielm3@github.com>
2022-05-21 14:35:43 -0300

    Appjs

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 let senhaElementosP = document.querySelector("div.gerador-senha p")
 const botaoGerarSenha = document.querySelector("div.gerador-senha button") 
 const caracteres = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","x","w","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","X","W","Y","0","1","2","3","4","5","6","7","8","9","@","#","$"]
-let senha
+
+let senha = ""
+
+
 //criando uma function gerars senha
+
 const gerarSenha = ()=>{
     
   for(let i = 1; i <= 12; i++){
@@ -11,7 +15,14 @@ const gerarSenha = ()=>{
     
   }
 
+  senhaElementos.textContent = senha
 
+  senha = ""
 
 }
-gerarSenha()
+
+
+botaoGerarSenha.onclick = ()=>{
+  gerarSenha() 
+
+}
